d12d2f56109bb824fe6aa2ba674115c1f0684a28
JLpensador <joaoluizborgesmorais@gmail.com>
2024-09-14 19:47:29 -0300

    Feat: Adiciona componentes de links para redes Sociais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TimerComponent } from './components/timer/timer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LinksComponent } from './components/links/links.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TimerComponent, HeaderComponent],
+  imports: [TimerComponent, HeaderComponent, LinksComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
