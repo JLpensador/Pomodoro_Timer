24538bd209fba25ed0f90c9f80b98dc0ce99fec1
JLpensador <joaoluizborgesmorais@gmail.com>
2024-09-27 18:51:05 -0300

    Feat: Adiciona Vercel Analytics injection ao Projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject as injectAnalytics } from '@vercel/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+injectAnalytics();
